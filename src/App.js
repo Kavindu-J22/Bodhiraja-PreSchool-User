fbef48ab00f60595e690d72c8a8449237b6836f9
Kavindu Jayasinghe <87432677+Kavindu-J22@users.noreply.github.com>
2024-12-04 07:42:23 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoadingScreen from './components/LoadingScreen.jsx'; // Import the loading screen component
 import Homepage from './pages/homepage/Homepage';
 import Login from './pages/Login/Login';
 import Teacher from './pages/Login/Teacher';
@@ -26,62 +27,70 @@ import ParentGuardianDetails from './pages/AdmissionForm/Home2/Components/father
 import GuardianForm from './pages/AdmissionForm/Home2/Components/guardian/guardian';
 import CombinedForm from './pages/AdmissionForm/Last/Last';
 import LearningMaterials from './pages/ProgressReport/LearningMaterials';
-import Payments from "./pages/ProgressReport/Payments";
+import Payments from './pages/ProgressReport/Payments';
 import ProgressChart from './pages/ProgressReport/ProgressChart';
 
 import ProgressReportPage from './pages/ProgressReport/ProgressReportPage';
-import AdmitionformNew from './pages/AdmissionForm/AdmitionFormNew.jsx'
+import AdmitionformNew from './pages/AdmissionForm/AdmitionFormNew.jsx';
 
 import AddGrage from './pages/NewParts/AddGrades.jsx';
 
-
 function App() {
+  const [isLoading, setIsLoading] = useState(true); // Manage loading state
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false); // Hide the loading screen after 3 seconds
+    }, 3000);
+
+    return () => clearTimeout(timer); // Cleanup timer
+  }, []);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/teacher" element={<Teacher />} />
-        <Route path="/student" element={<Student />} />
-        <Route path="/location" element={<Location />} />
-        <Route path="/ourschool" element={<OurSchool />} />
-        <Route path="/admission" element={<Admissions />} />
-        <Route path="/forgotpw" element={<ForgotPw />} />
-        {/* <Route path="/slidebar" element={<Sidebar />} /> */}
-        <Route path="/event" element={<EventsSlideshow />} />
-        <Route path="/learn" element={<LearningResources />} />
-        <Route path="/vid" element={<Tutorial/>}/>
-        <Route path="/story" element={<EmbeddedWebsite/>}/>
-        <Route path="/download" element={<Download />} />
-        <Route path="/downloadbutton" element={<DownloadButton />} />
-        <Route path="/resetpw" element={<ResetPw />} />
-        <Route path="/feedback" element={<Feedback/>} />
-        <Route path="/home1" element={<Home1/>} />
-        {/* <Route path="/home2" element={<Home2/>} /> */}
-        <Route path="/home3" element={<Home3/>} />
-        <Route path="/home4" element={<Home4/>} />
+    <>
+      {isLoading ? (
+        <LoadingScreen /> // Display the loading screen
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/teacher" element={<Teacher />} />
+            <Route path="/student" element={<Student />} />
+            <Route path="/location" element={<Location />} />
+            <Route path="/ourschool" element={<OurSchool />} />
+            <Route path="/admission" element={<Admissions />} />
+            <Route path="/forgotpw" element={<ForgotPw />} />
+            {/* <Route path="/slidebar" element={<Sidebar />} /> */}
+            <Route path="/event" element={<EventsSlideshow />} />
+            <Route path="/learn" element={<LearningResources />} />
+            <Route path="/vid" element={<Tutorial />} />
+            <Route path="/story" element={<EmbeddedWebsite />} />
+            <Route path="/download" element={<Download />} />
+            <Route path="/downloadbutton" element={<DownloadButton />} />
+            <Route path="/resetpw" element={<ResetPw />} />
+            <Route path="/feedback" element={<Feedback />} />
+            <Route path="/home1" element={<Home1 />} />
+            {/* <Route path="/home2" element={<Home2 />} /> */}
+            <Route path="/home3" element={<Home3 />} />
+            <Route path="/home4" element={<Home4 />} />
 
-        <Route path="/mother-info" element={<MotherInformation/>} />
-        <Route path="/parent-guardian-details" element={<ParentGuardianDetails/>} />
-        <Route path="/guardian" element={<GuardianForm/>} />
-        {/* <Route path="/last" element={<CombinedForm/>} /> */}
-        <Route path="/learning-materials" element={<LearningMaterials />} />
-        <Route path="/payments" element={<Payments />} />
-        <Route path="/progress-chart" element={<ProgressChart />} />
-        {/* Add other routes as needed */}
-        <Route path="/progress-report" element={<ProgressReportPage />} />
-        <Route path="/admitionformNew" element={<AdmitionformNew />} />
+            <Route path="/mother-info" element={<MotherInformation />} />
+            <Route path="/parent-guardian-details" element={<ParentGuardianDetails />} />
+            <Route path="/guardian" element={<GuardianForm />} />
+            {/* <Route path="/last" element={<CombinedForm />} /> */}
+            <Route path="/learning-materials" element={<LearningMaterials />} />
+            <Route path="/payments" element={<Payments />} />
+            <Route path="/progress-chart" element={<ProgressChart />} />
+            {/* Add other routes as needed */}
+            <Route path="/progress-report" element={<ProgressReportPage />} />
+            <Route path="/admitionformNew" element={<AdmitionformNew />} />
 
-        <Route path="/addGrage" element={<AddGrage />} />
-        
-
-
- 
-
-    
-
-      </Routes>
-    </Router>
+            <Route path="/addGrage" element={<AddGrage />} />
+          </Routes>
+        </Router>
+      )}
+    </>
   );
 }
 
